1e246259eea6ede41dbe5748b241d3eeff8c96f6
Tamme Schichler <tamme@schichler.dev>
2024-04-11 19:45:54 +0200

    Test reorganisation

--- flourish/tests/heap.rs
+++ flourish/tests/heap.rs
@@ -1,4 +1,4 @@
-use flourish::{shadow_clone, signal, subject, subscription, Signal, Subject, Subscription};
+use flourish::{shadow_clone, Signal, Subject, Subscription};
 
 #[test]
 fn use_constructors() {
@@ -28,38 +28,5 @@ fn use_constructors() {
 
     let _sub_c = Subscription::new(move || println!("{}", c.get())); // 8
     let _sub_d = Subscription::new(move || println!("{}", d.get())); // 2
-    a.set(4); // 7, then 1
-}
-
-#[test]
-fn use_macros() {
-    subject! {
-        let a := 1;
-        let b := 2;
-    }
-    let (b, set_b) = b.get_set();
-    signal! {
-        let c => a.get() + b();
-        let d => a.get() - b();
-        let aa => c.get() + d.get(); //TODO: Make this a cacheless signal.
-    }
-
-    {
-        subscription! {
-            let sub => println!("{}", aa.get()); // 2
-        }
-        set_b(2); // 2
-        a.set(0); // 0
-    } // drop sub
-
-    // These evaluate *no* closures!
-    a.set(2);
-    set_b(3);
-    a.set(5);
-
-    subscription! {
-        let sub_c => println!("{}", c.get()); // 8
-        let sub_d => println!("{}", d.get()); // 2
-    }
     a.set(4); // 7, then 1
 }
---
+++ flourish/tests/stack.rs
@@ -0,0 +1,34 @@
+use flourish::{signal, subject, subscription};
+
+#[test]
+fn use_macros() {
+    subject! {
+        let a := 1;
+        let b := 2;
+    }
+    let (b, set_b) = b.get_set();
+    signal! {
+        let c => a.get() + b();
+        let d => a.get() - b();
+        let aa => c.get() + d.get(); //TODO: Make this a cacheless signal.
+    }
+
+    {
+        subscription! {
+            let sub => println!("{}", aa.get()); // 2
+        }
+        set_b(2); // 2
+        a.set(0); // 0
+    } // drop sub
+
+    // These evaluate *no* closures!
+    a.set(2);
+    set_b(3);
+    a.set(5);
+
+    subscription! {
+        let sub_c => println!("{}", c.get()); // 8
+        let sub_d => println!("{}", d.get()); // 2
+    }
+    a.set(4); // 7, then 1
+}
