bda3f30424415b404181a04c86fd95e2732ce319
Tamme Schichler <tamme@schichler.dev>
2024-06-29 10:57:40 +0200

    Added test for uncached signal dependency tracking

---
+++ flourish/tests/uncached_dependency_tracking.rs
@@ -0,0 +1,99 @@
+use std::sync::Mutex;
+
+use flourish::{Signal, Subject, Subscription};
+mod _validator;
+use _validator::Validator;
+
+#[test]
+fn both() {
+    //FIXME: Split this once the runtime can handle threading.
+    {
+        let v = &Validator::new();
+
+        let (a, set_a) = Subject::new(()).into_get_set();
+        let (b, set_b) = Subject::new(()).into_get_set();
+        let (c, set_c) = Subject::new(()).into_get_set();
+
+        let roundabout = Signal::computed_uncached_mut({
+            let mut angle = 0;
+            move || {
+                match angle {
+                    0 => a(),
+                    1 => b(),
+                    2 => c(),
+                    _ => unreachable!(),
+                }
+                angle = (angle + 1) % 3;
+            }
+        });
+        v.expect([]);
+
+        let _a = Subscription::computed(|| {
+            v.push('a');
+            roundabout.get()
+        });
+        v.expect(['a']);
+        let _b = Subscription::computed(|| {
+            v.push('b');
+            roundabout.get()
+        });
+        v.expect(['b']);
+
+        // There are two subscriptions, so each "hit" advances twice.
+
+        set_b(());
+        v.expect(['a', 'b']);
+
+        set_b(());
+        set_c(());
+        v.expect([]);
+
+        set_a(());
+        v.expect(['a', 'b']);
+    }
+    {
+        let v = &Validator::new();
+
+        let (a, set_a) = Subject::new(()).into_get_set();
+        let (b, set_b) = Subject::new(()).into_get_set();
+        let (c, set_c) = Subject::new(()).into_get_set();
+
+        let roundabout = Signal::computed_uncached({
+            let angle = Mutex::new(0);
+            move || {
+                let mut angle = angle.lock().unwrap();
+                match *angle {
+                    0 => a(),
+                    1 => b(),
+                    2 => c(),
+                    _ => unreachable!(),
+                }
+                *angle = (*angle + 1) % 3;
+            }
+        });
+        v.expect([]);
+
+        let _a = Subscription::computed(|| {
+            v.push('a');
+            roundabout.get()
+        });
+        v.expect(['a']);
+        let _b = Subscription::computed(|| {
+            v.push('b');
+            roundabout.get()
+        });
+        v.expect(['b']);
+
+        // There are two subscriptions, so each "hit" advances twice.
+
+        set_b(());
+        v.expect(['a', 'b']);
+
+        set_b(());
+        set_c(());
+        v.expect([]);
+
+        set_a(());
+        v.expect(['a', 'b']);
+    }
+}
