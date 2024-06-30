bc2c83a185ae4f1c05ceea27eea93597f1bc9fc7
Tamme Schichler <tamme@schichler.dev>
2024-06-30 14:16:19 +0200

    `filtered`

--- flourish-extra/src/future.rs
+++ flourish-extra/src/future.rs
@@ -1,7 +1,13 @@
 //! `Source` <=> `Future` adapters.
 
+use std::{
+    mem::{self, MaybeUninit},
+    pin::Pin,
+    sync::Arc,
+};
+
 use async_lock::OnceCell;
-use flourish::{signals_helper, SignalRuntimeRef, Source, SubscriptionSR};
+use flourish::{shadow_clone, signals_helper, SignalRuntimeRef, Source, SubscriptionSR, Update};
 
 pub async fn skipped_while<'a, T: 'a + Send + Sync + Copy, SR: 'a + SignalRuntimeRef>(
     source: impl 'a + Source<SR, Value = T>,
@@ -47,4 +53,38 @@ pub async fn skipped_while_cloned<'a, T: 'a + Send + Sync + Clone, SR: 'a + Sign
         once.wait().await;
     }
     sub
+}
+
+pub async fn filtered<'a, T: 'a + Send + Sync + Copy, SR: 'a + SignalRuntimeRef>(
+    source: impl 'a + Source<SR, Value = T>,
+    mut test: impl 'a + Send + FnMut(&T) -> bool,
+) -> SubscriptionSR<'a, T, SR> {
+    let runtime = source.clone_runtime_ref();
+    let once = Arc::new(OnceCell::<()>::new());
+    let sub = SubscriptionSR::folded_with_runtime(
+        MaybeUninit::uninit(),
+        {
+            shadow_clone!(once);
+            move |value| {
+                let next = unsafe { Pin::new_unchecked(&source) }.get();
+                if test(&next) {
+                    if once.is_initialized() {
+                        *unsafe { value.assume_init_mut() } = next;
+                    } else {
+                        value.write(next);
+                        once.set_blocking(()).expect("unreachable");
+                    }
+                    Update::Propagate
+                } else {
+                    Update::Halt
+                }
+            }
+        },
+        runtime,
+    );
+    once.wait().await;
+
+    unsafe {
+        mem::transmute::<SubscriptionSR<'a, MaybeUninit<T>, SR>, SubscriptionSR<'a, T, SR>>(sub)
+    }
 }
