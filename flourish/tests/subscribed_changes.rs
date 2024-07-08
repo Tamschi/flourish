74acd83cf67c9615baac4b52541a55e4a30e35ad
Tamme Schichler <tamme@schichler.dev>
2024-07-08 22:34:52 +0200

    Subscription notification test

---
+++ flourish/tests/subscribed_changes.rs
@@ -0,0 +1,59 @@
+use flourish::{shadow_clone, Provider, SourcePin as _, Subscription};
+mod _validator;
+use _validator::Validator;
+
+#[test]
+fn inherent() {
+	let v = &Validator::new();
+
+	let a = Provider::new((), |status| v.push(status));
+	let s = a.to_signal();
+	drop(a);
+	v.expect([]);
+
+	let s = s.try_subscribe().unwrap();
+	v.expect([true]);
+
+	drop(s);
+	v.expect([false]);
+}
+
+#[test]
+fn dependent() {
+	let v = &Validator::new();
+
+	let a = Provider::new((), |status| v.push(status));
+	v.expect([]);
+
+	let s = Subscription::computed({
+		shadow_clone!(a);
+		move || a.get()
+	});
+	v.expect([true]);
+
+	drop(a);
+	v.expect([]);
+
+	drop(s);
+	v.expect([false]);
+}
+
+#[test]
+fn dependent_reversed() {
+	let v = &Validator::new();
+
+	let a = Provider::new((), |status| v.push(status));
+	v.expect([]);
+
+	let s = Subscription::computed({
+		shadow_clone!(a);
+		move || a.get()
+	});
+	v.expect([true]);
+
+	drop(s);
+	v.expect([false]);
+
+	drop(a);
+	v.expect([]);
+}
