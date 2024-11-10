c00c3df61d75ab1977735413adcdf909e88df189
Tamme Schichler <tamme@schichler.dev>
2024-11-10 12:17:27 +0100

    Documented slot.rs

--- isoprenoid/src/slot.rs
+++ isoprenoid/src/slot.rs
@@ -1,13 +1,15 @@
 //! [`Slot`] is used in certain callbacks to ensure initialisation.
 //!
-//! [`Slot::write`] yield a [`Token`] with the same (invariant) lifetime.
+//! [`Slot::write`] and [`Slot::assume_init`] yield a [`Token`] with the same invariant lifetime.
+//! Iff that lifetime is sufficiently unique (i.e. because it's transient in a callback),
+//! each [`Token`] proves that one matching [`Slot`] has been written to.
 
 use core::{marker::PhantomData, mem::MaybeUninit};
 
-/// Must be written to before the closure returns.
+/// Must be written to to create one matching [`Token`].
 pub struct Slot<'a, T>(&'a mut MaybeUninit<T>, PhantomData<&'a mut &'a mut ()>);
 
-/// Proof that a [`Slot`] was written to.
+/// Proof that one matching [`Slot`] was written to.
 pub struct Token<'a>(PhantomData<&'a mut &'a mut ()>);
 
 impl<'a, T> Slot<'a, T> {
@@ -15,18 +17,22 @@ impl<'a, T> Slot<'a, T> {
 		Self(target, PhantomData)
 	}
 
-	/// Writes `value` while consuming the [`Slot`], yielding a [`Token`].
+	/// Writes `value` while consuming this [`Slot`], yielding a matching [`Token`].
 	pub fn write(self, value: T) -> Token<'a> {
 		self.0.write(value);
 		Token(PhantomData)
 	}
 
 	/// Provides exclusive access to the underlying [`MaybeUninit`].
-	pub unsafe fn get_uninit(&mut self) -> &mut MaybeUninit<T> {
+	pub fn get_uninit(&mut self) -> &mut MaybeUninit<T> {
 		&mut *self.0
 	}
 
-	/// Creates a [`Token`] from this [`Slot`].
+	/// Converts this [`Slot`] into a matching [`Token`].
+	///
+	/// # Safety
+	///
+	/// The memory this [`Slot`] points to **must** have been initialised with a valid `T`.
 	pub unsafe fn assume_init(self) -> Token<'a> {
 		Token(PhantomData)
 	}
